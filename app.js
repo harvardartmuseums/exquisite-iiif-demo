c7f430d204dd26c5e45b12863cb4e1194cfcac82
Jeff Steward <steward.jeff@gmail.com>
2016-10-14 19:07:00 -0400

    move portaits functions to its own routes file

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var portraits = require('./routes/portraits');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/portraits', portraits);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
