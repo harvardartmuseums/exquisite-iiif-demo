c197449683fcb409b190d489960d83a50e8f9524
Jeff Steward <steward.jeff@gmail.com>
2016-10-14 19:12:39 -0400

    remove unused routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-var apikey = process.env.APIKEY;
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Exquisite IIIF Demo | Harvard Art Museums' });
