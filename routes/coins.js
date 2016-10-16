1dccca1d80ffeabd553d0f35d7c91d3c83f0a2b6
Jeff Steward <steward.jeff@gmail.com>
2016-10-16 19:12:41 -0400

    initial version of coin splitter

--- routes/coins.js
+++ routes/coins.js
@@ -2,11 +2,42 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-router.get('/', function(req, res, next) {
-  var coins = [195903]
-  var imageLeftURL = 'http://ids.lib.harvard.edu/ids/iiif/20411130/0,0,1240,1174/full/0/native.jpg';
+var apikey = process.env.APIKEY;
+var objectURL = 'http://api.harvardartmuseums.org/object';
+var queryString = {
+    apikey: apikey, 
+    sort: 'random', 
+    classification: 'Coins', 
+    fields: 'title,primaryimageurl,images', 
+    hasimage: 1, 
+    size: 2
+  };
 
-  res.render('coins', { title: 'Exquisite IIIF Demo | Harvard Art Museums' });
+router.get('/', function(req, res, next) {
+	var imageParts = [];
+
+	request(objectURL, {qs: queryString}, function(error, response, body) {
+		var data = JSON.parse(body);  	
+		if (!data.error) {
+			var imageInfoURL = data.records[0].images[0].iiifbaseuri + '/info.json';
+	        request(imageInfoURL, function(error, response, body) {
+	        	imageInfo = JSON.parse(body);
+				imageParts.push(imageInfo['@id'] + '/0,0,' + Math.floor(imageInfo.width/2) + ',' + Math.floor(imageInfo.height/2) + '/full/0/native.jpg');
+
+				imageInfoURL = data.records[1].images[0].iiifbaseuri + '/info.json';
+		        request(imageInfoURL, function(error, response, body) {
+		        	imageInfo = JSON.parse(body);
+					imageParts.push(imageInfo['@id'] + '/0,' + Math.floor(imageInfo.height/2) + ',' + Math.floor(imageInfo.width/2) + ',' + Math.floor(imageInfo.height/2) + '/full/0/native.jpg');
+		
+					res.render('coins', { 
+						title: 'Exquisite IIIF Demo | Harvard Art Museums', 
+						image_top: imageParts[0],
+						image_bottom: imageParts[1]
+					});
+				});
+			});
+		}
+	});
 });
 
 module.exports = router;
