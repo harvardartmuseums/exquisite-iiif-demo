ca4594d2023a47934ac0461f4e0e353faff817dd
Jeff Steward <steward.jeff@gmail.com>
2016-10-15 22:33:26 -0400

    add bootstrap to handle layouts

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
 
 app.use('/', routes);
 app.use('/portraits', portraits);
